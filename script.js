2930934d0125f7e9b928c557c81053c77c03baf1
Johnny Austen <johnnydausten@gmail.com>
2021-03-10 14:44:05 +0000

    Add Secondary Chart Data

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ const alertDivs = document.querySelectorAll(".content__alert");
 const notificationsButton = document.getElementById("notifications--button");
 const chartSelector = document.querySelector(".chart--selector").autofocus;
 
-function insertGraph(location, type) {
-  var ctx = document.getElementById(location).getContext("2d");
+const hourlySelector = document.querySelector("#hourly--selector");
+const dailySelector = document.querySelector("#daily--selector");
+const weeklySelector = document.querySelector("#weekly--selector");
+const monthlySelector = document.querySelector("#monthly--selector");
+
+const userSearch = document.querySelector("#user--search");
+
+function insertGraph(data, labels) {
+  var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
-    type: type,
+    type: "line",
     data: {
-      labels: ["9AM", "11AM", "1PM", "3PM", "6PM", "9PM"],
+      labels: labels,
       datasets: [
         {
           label: "Website Traffic Throughout the Day",
-          data: [1, 2, 7, 10, 14, 25],
+          data: data,
           lineTension: 0,
           pointBackgroundColor: "white",
           // pointBorderColor: "rgba(150, 132, 208, 1)",
@@ -98,13 +105,93 @@ function insertPie(location, type) {
   });
 }
 
-insertGraph("myChart", "line");
+const hourlyData = [1, 2, 7, 10, 14, 25];
+const hourlyLabels = ["9AM", "11AM", "1PM", "3PM", "6PM", "9PM"];
+
+const dailyData = [50, 60, 70, 40, 50, 80, 100];
+const dailyLabels = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const weeklyData = [700, 900, 300, 50, 1000];
+const weeklyLabels = ["Wk1", "Wk2", "Wk3", "Wk4", "Wk5"];
+
+const monthlyData = [
+  2000,
+  2200,
+  2400,
+  2000,
+  2500,
+  2600,
+  4000,
+  3000,
+  7000,
+  6000,
+  3000,
+  1100,
+];
+const monthlyLabels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+insertGraph(hourlyData, hourlyLabels);
 insertBar("daily__traffic__chart", "bar");
 insertPie("mobile__users__chart", "doughnut");
 
-// alertButton.addEventListener("click", () => {
-//   document.querySelector(".content__alert").style.display = "none";
-// });
+hourlySelector.addEventListener("click", () => {
+  insertGraph(hourlyData, hourlyLabels);
+});
+
+dailySelector.addEventListener("click", () => {
+  insertGraph(dailyData, dailyLabels);
+});
+
+weeklySelector.addEventListener("click", () => {
+  insertGraph(weeklyData, weeklyLabels);
+});
+
+monthlySelector.addEventListener("click", () => {
+  insertGraph(monthlyData, monthlyLabels);
+});
+
+const validateForm = () => {
+  let user = document.forms["messageForm"]["username"].value;
+  let message = document.forms["messageForm"]["message"].value;
+  if (!user) {
+    alert("Please enter a user");
+    return false;
+  } else if (!message) {
+    alert("Please enter a message!");
+    return false;
+  }
+  alert("Success!");
+};
+
+users = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"];
+
+userSearch.addEventListener("input", () => {
+  console.log(userSearch.value);
+  if (users.includes(userSearch.value)) {
+    console.log("GOOD!");
+  }
+});
 
 for (let x = 0; x < alertButtons.length; x++) {
   alertButtons[x].addEventListener("click", () => {
